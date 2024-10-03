c1e704cbab8c8e63e03c9fa0f90183bad90a8b8f
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-03 15:55:02 +0530

    third

---
+++ project2/components/employeeData.jsx
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+
+//component CollegeData
+const EmployeeDta = ()=>{
+  return(
+    <View>
+      <Text style={{fontSize: 20}}>UserId: 778</Text>
+      <Text style={{fontSize: 20}}>Department: SDE</Text>
+      <Text style={{fontSize: 20, borderBottomWidth: 1}}>Address: Bangalore</Text>
+    </View>
+  );
+}
+
+export default EmployeeDta;
+
+// borderBottomWidth --> Thickness of the line
