b7af6d48a43968bc1218bfe0301b3545ed8fa7c5
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-08 16:12:36 +0530

    second Commit

--- project12/components/RadioButton.js
+++ project12/components/RadioButton.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import { StyleSheet, TouchableHighlight } from "react-native";
 
 const RadioButton = () =>{
+
+  const [selectRadio, setSelectradio]  = useState(1)
   return(
     <View style = {styles.main}>
       //khudke radio button nahi externally use kar sakte hai
-      <TouchableHighlight>
+      <TouchableHighlight onPress={()=>{setSelectradio(1)}}>
         <View style = {styles.wrapper}>
           <View style = {styles.radio}>
+            {selectRadio  === 1? <View style= {styles.radioBg}></View> : null }
+            
             <Text style = {styles.radioText}>Radio 1</Text>
           </View>
         </View>
       </TouchableHighlight>
+
+      <TouchableHighlight onPress={setSelectradio(2)}>
+        <View style = {styles.wrapper}>
+          <View style = {styles.radio}>
+          {selectRadio  === 2? <View style= {styles.radioBg}></View> : null }
+            <View style= {styles.radioBg}></View>
+            <Text style = {styles.radioText}>Radio 1</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+
     </View>
   )
 }
@@ -35,6 +51,13 @@ const styles = StyleSheet.create({
   wrapper:{
     flexDirection:'row',
     alignItems:'center'
+  },
+  radioBg:{
+    backgroundColor:'black',
+    height:28,
+    width:28,
+    borderRadius:20,
+    margin:4
   }
 })
 export default RadioButton;
