e2cf862dc33cffa2944d4e4967ab777799985f29
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-09 17:09:53 +0530

    First Commit

---
+++ project14/components/StackNavigation.js
@@ -0,0 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Button, Text,View} from 'react-native'
+
+
+const Stack = createNativeStackNavigator();
+const StackNavigation = () =>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+   
+  )
+}
+
+const Home = () =>{
+  return(
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+const Login = (props) =>{
+  return(
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text style={{fontSize:30}}>Login Screen</Text>
+      <Button title='GO To Home Page' onPress={()=>props.navigation.navigate("Home")}/>
+    </View>
+  )
+}
+export default StackNavigation;
