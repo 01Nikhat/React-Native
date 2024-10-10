0d0600c7d34cb8e5bc5c6506470453e70a93f56c
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-10 18:04:36 +0530

    First Commit

---
+++ project17/App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import ApiIntegrate from './components/ApiIntegrate';
+
+
+function App(): React.JSX.Element {
+  
+
+  
+  return (
+    <ApiIntegrate />
+    
+  );
+}
+
+
+
+export default App;
