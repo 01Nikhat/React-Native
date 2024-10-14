84947e3833e712a47f3ee8c9627a017c4a6314fc
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-14 17:57:35 +0530

    First Commit

--- project17/components/DeleteAPI.js
+++ project17/components/DeleteAPI.js
@@ -1,11 +1,100 @@
-import { View } from "react-native"
 
-const DeleteAPI = () =>{
-  const DeleteMethod=()=>{
-    
+
+import { useEffect, useState } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  ScrollView
+} from 'react-native';
+
+
+const DeleteApi = () => {
+  const [data, setData] = useState([]); //array of objects
+
+  const getApiData = async () =>{
+    const url = 'http://10.0.2.2:3000/users';
+    const result = await fetch(url); //by default method: 'GET'
+    const dataFetched = await result.json();
+    // console.warn(dataFetched); //just to see the object
+
+    if(dataFetched){ //or can use 'result' in condition
+      setData(dataFetched);
+    }
   }
-  return(<View>
 
-  </View>)
-}
-export default DeleteAPI;
+  /*
+    url: http://10.0.2.2:3000/users
+    id: 5 (say)
+    therefore,
+   `${url}/${id}` ---> http://10.0.2.2:3000/users/5
+  */
+  const deleteUser = async (id) =>{
+    const url = 'http://10.0.2.2:3000/users';
+
+    console.error(`${url}/${id}`); //just to see
+    const result = await fetch(`${url}/${id}`, {
+      method: 'DELETE'
+    }); 
+    const dataFetched = await result.json();
+    if(dataFetched){ //or can use 'result' in condition
+      console.warn(`UserId: ${id} Deleted !!!!!`);
+    }
+    getApiData(); //to refresh on the screen, so re-render will occur again
+  }
+
+  useEffect(()=>{
+    getApiData(); //to call the getApiData function
+  },[]);
+
+  return (
+    <ScrollView style={{flex:1}}>
+
+      <View style={styles.wrapper}>
+        <View style={{ flex: 2}}><Text style={{fontSize: 20}}>Name</Text></View>
+        <View style={{ flex: 2 }}><Text style={{fontSize: 20}}>Age</Text></View>
+        <View style={{ flex: 2 }}><Text style={{fontSize: 20}}>Operations</Text></View>
+      </View>
+
+      {
+        data.length !== 0 ?
+        data.map((item) => (
+          <View style={styles.wrapper} key={item.id}>
+            <View style={{ flex: 2 }}><Text>{item.name}</Text></View>
+            <View style={{ flex: 1 }}><Text>{item.age}</Text></View>
+            <View style={styles.buttonContainer}>
+              {/* passing id so we can delete that particular person's object */}
+              <Button title='delete' onPress={()=>deleteUser(item.id)} />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button title='update' />
+            </View>
+          </View>
+        ))
+        : null
+      }
+
+    </ScrollView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'orange',
+    margin: 5,
+    padding: 10,
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flex: 1,
+    marginHorizontal: 5,   // Add space around each button
+    paddingVertical: 5,    // Add padding above and below to increase size
+  },
+})
+
+export default DeleteApi;
--- project17/components/ListApi.js
+++ project17/components/ListApi.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
-import { Button, Modal, StyleSheet, Text, View } from "react-native"
+import { Button, Modal, StyleSheet, Text, TextInput, View } from "react-native"
 
 const ListApi = () =>{
   const [data,setData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedUser,setSelectedUser] = useState(undefined);
+
+  //here we are fetching data from API 
+  //Afetr fetching data it's showing on the UI
 
   const getAPIData = async() =>{
 
     const url = 'http://10.0.2.2:3000/users';
     const result = await fetch(url);
     const dataObjFetched = await result.json();
-    console.warn(dataObjFetched);
+   // console.warn(dataObjFetched);
     
     if (dataObjFetched) {
         setData(dataObjFetched)
-      
+       // console.warn("Fetched data length:", dataObjFetched.length);
     }
   }
 
+// first we have to fetch user id so that we know which user want to delete the data 
+//after fetching userid we will call the delete method from API
+//jaise delete hoga waise getAPIData function call hoga jisse data API ke saath UI par bhi deleted deekhega 
   const deleteUser = async (id) =>{
     const url = 'http://10.0.2.2:3000/users';
    let result = await fetch(`${url}/${id}`,{ method:"DELETE",});
@@ -28,10 +36,20 @@ const ListApi = () =>{
 
   }
 
+  
+
   useEffect(()=>{
     getAPIData();
   },[]);
 
+  //Modal updation Operation
+
+  const updateModalUser = (data) =>{
+    setShowModal(true);
+    setSelectedUser(data);
+  
+  }
+// Front UI design part
   return(
     <View style={styles.container}>
 
@@ -48,23 +66,85 @@ const ListApi = () =>{
 
           <View style={{flex:1}}><Text >{item.name}</Text></View>
           <View style={{flex:1}}><Text >{item.age}</Text></View>
-          <View style={{flex:1}}><Button title="Update"></Button></View>
+          <View style={{flex:1}}><Button title="Update" onPress={()=>updateModalUser(item)}></Button></View>
           <View style={{flex:1}}><Button title="Delete" onPress={()=>deleteUser(item.id)}></Button></View>
           
 
       </View>):null}
       <Button onPress={getAPIData} title="Click" />
 
-      <Modal>
-        <View>
-          <View>
-            <Text>Dummy Text</Text>
-          </View>
-        </View>
+      <Modal visible={showModal} transparen={true}>
+        <UpdateModal setShowModal = {setShowModal} selectedUser={selectedUser}
+        getAPIData={getAPIData} />
+       
       </Modal>
     </View>
   )
 }
+
+
+
+// in this operation we are making 3 input and initially jo user update par click karega uska data initial 
+// input value mai show karega then waha upadate kar sakte hai
+
+const UpdateModal = (props) =>{
+  //console.warn(props.selectedUser);
+
+    
+  const [name,setName] = useState('');
+  const [age,setAge] = useState('');
+  const [email,setEmail] = useState('');
+
+  useEffect(()=>{
+    if (props.selectedUser) {
+      setName(props.selectedUser.name);
+      setAge(props.selectedUser.age.toString());
+      setEmail(props.selectedUser.email);
+      
+    }
+  },[props.selectedUser]);
+
+  const updateUser = async() =>{
+
+    const url = 'http://10.0.2.2:3000/users';
+    const id = props.selectedUser.id
+    let result = await fetch(`${url}/${id}`,{ 
+      method:"Put",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({name,age,email})
+    });
+    result = await result.json();
+    if (result) {
+      console.warn("Data Saved");
+      props.getAPIData(); // Fetch updated data after saving
+      props.setShowModal(false); // Close the modal after save
+    }
+
+    console.warn(name,email,age);
+  
+  }
+  
+  
+ return( <View style={styles.centeredView}>
+      <View style={styles.modalView}>
+
+        <TextInput style={styles.input} placeholder="Enter Name" 
+          value={name} onChangeText={(text)=>setName(text)}/>
+        <TextInput style={styles.input} 
+         placeholder="Enter Email" value={age} onChangeText={(text)=>setAge(text)}/>
+        <TextInput style={styles.input} 
+         placeholder="Enter Age"  value={email} onChangeText={(text)=>setEmail(text)}/>
+        <View style={{marginBottom:15}}><Button title="Save" onPress={updateUser}/></View>
+        <Button title="Close" onPress={()=>props.setShowModal(false)}/>
+      </View>
+  </View>)
+
+}
+
+//Css part 
+
 const styles = StyleSheet.create({
   container:{
     flex:1
@@ -76,6 +156,28 @@ const styles = StyleSheet.create({
     backgroundColor:'orange',
     margin:5,
     padding:3
+  },
+  centeredView :{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  modalView :{
+    backgroundColor:'#fff',
+    borderRadius:10,
+    padding:60,
+    shadowColor:'#000',
+    shadowOpacity:0.70,
+    elevation:5,
+    margin:5
+
+  },
+  input:{
+    borderWidth:1,
+    borderColor:'skyblue',
+    width:300,
+    marginBottom:15,
+    fontSize:20
   }
 })
 export default ListApi;
