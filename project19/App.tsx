4e3b533ebed40b699a6114c6cc37cbb4a8ab7bd2
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-15 18:31:13 +0530

    First Commit

--- project19/App.tsx
+++ project19/App.tsx
@@ -7,13 +7,15 @@
 
 import React from 'react';
 import RefUse from './components/RefUse';
+import Async_Storage from './components/Async_Storage';
 
 
 function App(): React.JSX.Element {
   
 
   return (
-    <RefUse />
+   // <RefUse />
+   <Async_Storage />
    
   );
 }
