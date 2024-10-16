c827322d7e2f2227fa480fa84b8c087a879bde96
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-10-16 14:51:11 +0530

    Second Commit

--- project20/components/Header.js
+++ project20/components/Header.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, View,Text } from "react-native"
+import { useSelector } from "react-redux"
 
 const Header = () =>{
+  const cartData = useSelector((state)=>state.reducer);
+  //console.warn(cartData);
+  const [cartItem,setCartItem] = useState(0);
+
+  useEffect(()=>{
+    setCartItem(cartData.length)
+  },[cartData])
+  
   return(
     <View >
-      <Text style = {{fontSize:30, textAlign:'right',padding:10,backgroundColor:'orange'}}>0</Text>
+      <Text style = {{fontSize:30, textAlign:'right',padding:10,backgroundColor:'orange'}}>
+        <View style={{backgroundColor:'yellow',borderRadius:20,height:40,width:40}}>
+          <Text style={{fontSize:30}}> {cartItem}</Text>
+        </View>
+       </Text>
     </View>
   )
 }
--- project20/components/Product.js
+++ project20/components/Product.js
@@ -1,11 +1,32 @@
 import { StyleSheet, View,Text,Image,Button } from "react-native"
 
-import { addToCart } from "./redux/action";
-import { useDispatch } from "react-redux";
+import { addToCart ,removeFromCart } from "./redux/action";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Product = (props) =>{
 
+  const [isAddToCart,setIsAddToCart] = useState(false);
+  const cartItem = useSelector((state)=>state.reducer);
+  useEffect(()=>{
+    let result = cartItem.filter((element)=>{
+        return element.name === item.name
+    });
+    if(result.length){
+      setIsAddToCart(true);
+    }
+    else{
+      setIsAddToCart(false)
+    }
+  },[cartItem]);
+
+  const handleRemoveFromCart = (item) =>{
+      dispatch(removeFromCart(item.name))
+
+  }
+
   const dispatch = useDispatch();
+
   const handleAddToCart = (item) =>{
     dispatch(addToCart(item))
 
@@ -13,12 +34,16 @@ const Product = (props) =>{
 
   const item = props.item;
   return(
-    <View style={{alignItems:'center' , borderBottomColor:'orange',borderBottomWidth:2,padding:10}}>
+    <View style={{alignItems:'center' , borderBottomColor:'orange',borderBottomWidth:2,padding:10,margin:50}}>
         <Image style={{ width: 150, height: 150 }} source={{ uri: item.image }} />
           <Text style={{fontSize:24}}>{item.name}</Text>
           <Text style={{fontSize:24}}>{item.price}</Text>
           <Text style={{fontSize:24}}>{item.color}</Text>
-          <Button title="Add To Cart" onPress={()=>handleAddToCart(item)}></Button>
+          {
+            isAddToCart? <Button title="Remove To Cart" onPress={()=>handleRemoveFromCart(item)}></Button>
+            : <Button title="Add To Cart" onPress={()=>handleAddToCart(item)}></Button>
+          }
+          
        </View>
   )
 }
